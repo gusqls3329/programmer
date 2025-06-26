fabe49eeb9a2fa99f72de1fc248e85d70d35b42e
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-06-27 08:52:42 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -1,10 +1,8 @@
 
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for (int i=0; i<myString.length(); i++){
-            answer += Character.toLowerCase(myString.charAt(i));
-        }
+        String answer = myString.toLowerCase();
+        
         
         return answer;
     }
