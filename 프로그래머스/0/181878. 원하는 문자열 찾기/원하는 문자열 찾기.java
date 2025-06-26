6ac42d24da36b10413662dc35854ed9ada379f68
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-06-27 08:56:27 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.75 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        myString = myString.toUpperCase();
+        pat = pat.toUpperCase();
+        
+        if( myString.contains(pat)){
+            return 1;
+        } else{
+            return answer;
+        }
+    }
+}
