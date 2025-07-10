226d75ec5e91b82ec9966a51b59f2cf5e78021b1
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-07-10 15:15:56 +0900

    [level 0] Title: 배열 만들기 3, Time: 2.01 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int l = (intervals[0][1]-intervals[0][0]+1)+(intervals[1][1]-intervals[1][0]+1);
+        int[] answer = new int[l];
+        int a=0;
+        for (int i=intervals[0][0];i<=intervals[0][1];i++){
+            answer[a]=arr[i];
+            a++;
+        }
+        for (int i=intervals[1][0];i<=intervals[1][1];i++){
+            answer[a]=arr[i];
+            a++;
+        }
+        return answer;
+    }
+}
