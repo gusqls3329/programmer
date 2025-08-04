23529cbca295ef19ad407f3e6b5a1b93db74a4a7
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-08-04 10:02:22 +0900

    [level 0] Title: 배열 만들기 6, Time: 35.16 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList <Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++){
+            if(i==0){
+                list.add(arr[0]);
+            }else if(list.isEmpty() || list.get(list.size()-1) != arr[i]) {
+                list.add(arr[i]);
+            }else{
+                list.remove(list.size()-1) ;
+            }
+        }
+        if(list.isEmpty()){
+            return new int[]{-1};
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
