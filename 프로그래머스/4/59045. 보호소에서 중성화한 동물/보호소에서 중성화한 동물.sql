f35e41685881d1d7f3c35ffb4ee3ad6a0c6fbfa7
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-08-11 16:31:06 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT i.ANIMAL_ID, i.ANIMAL_TYPE, i.name
+from ANIMAL_INS i
+join ANIMAL_OUTS o
+on i.ANIMAL_ID = o.ANIMAL_ID
+WHERE i.SEX_UPON_INTAKE  != o.SEX_UPON_OUTCOME 
+order by i.ANIMAL_ID 
+;
+
