603fb62810f47da4d0cbcf9f5fede7d66bafbde4
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-08-20 14:03:12 +0900

    [level 0] Title: 특이한 정렬, Time: 0.88 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        Arrays.sort(numlist);
+        int tmp = 0;
+        for(int i=0; i<numlist.length; i++){
+            for(int j=0; j<numlist.length; j++){
+                if(Math.abs(numlist[i]-n)<=Math.abs(numlist[j]-n)){
+                    tmp = numlist[i];
+                    numlist[i] = numlist[j];
+                    numlist[j] = tmp;
+                }
+            }
+        }
+        
+        return numlist;
+    }
+}
