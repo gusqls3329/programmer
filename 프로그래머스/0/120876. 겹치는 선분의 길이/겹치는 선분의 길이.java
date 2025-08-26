86826082270a8856013db58da2f2b1a08c64b070
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-08-26 09:48:45 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.44 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] lines) {
+        HashMap <Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<lines.length; i++){
+            for(int j=lines[i][0]; j<lines[i][1]; j++){
+                map.put(j, map.getOrDefault(j,0)+1);
+            }
+        }
+        int answer = 0;
+        for(Integer key : map.keySet()){
+            if(map.get(key) > 1){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
