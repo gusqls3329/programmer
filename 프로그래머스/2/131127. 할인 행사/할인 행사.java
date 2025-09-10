7f7bc4f01d7e0181f483c1353dafda430dc20f20
Hyunbin Kim <146905633+gusqls3329@users.noreply.github.com>
2025-09-10 14:36:49 +0900

    [level 2] Title: 할인 행사, Time: 77.35 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            map.put(want[i], number[i]);
+        }
+
+        for (int i = 0; i <= discount.length - 10; i++) {
+            HashMap<String, Integer> countmap = new HashMap<>();
+            for (int j = i; j < i + 10; j++) {
+                countmap.put(discount[j], countmap.getOrDefault(discount[j], 0) + 1);
+            }
+
+            if (map.equals(countmap)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
